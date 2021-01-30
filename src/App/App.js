c71a563a481c9275f946057580be7281c2884ba4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-29 19:47:22 -0600

    restructured App into folders

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { main } from './App.module.css';
+
+function App() {
+  return (
+    <div className={main}>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
