730f3ba815f546aa648d550401242cf66c434611
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-30 14:25:59 -0600

    created home route and home component structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,23 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+import Home from '../Home/Home';
 import { main } from './App.module.css';
 
 const App = () => {
   return (
     <div className={main}>
       <p>Hello World</p>
+      <Router>
+          <Link />
+          <Link />
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/radio"/>
+        </Switch>  
+      </Router>
     </div>
-  );
-}
+  )
+};
 
 export default App;
