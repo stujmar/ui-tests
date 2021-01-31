bca061655059fb352d960c4dc3d9ffcd49101d95
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-30 22:09:54 -0600

    build out code structure of Radio comp and Link module.css

---
+++ src/Radio/Radio.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {body} from './Radio.module.css'
+
+const Radio = () => {
+    return (
+        <div className={body} >Radio</div>
+    )
+}
+
+export default Radio;
