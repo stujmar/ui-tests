db2812baf3e58ec18a6299450ff54cd2c72b458b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-31 18:30:57 -0600

    added nameForm comp to files and to app router

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Home from '../Home/Home';
+import NameForm from '../NameForm/NameForm';
 import NavBar from '../NavBar/NavBar';
 import Radio from '../Radio/Radio';
 import { main } from './App.module.css';
@@ -10,13 +11,14 @@ const App = () => {
   return (
     <div className={main}>
       <p>Hello World</p>
+      <button>within react but outside of Router</button>
       <Router>
           <NavBar />
-          {/* <Link to="/">Home From App</Link>
-          <Link to="/radio">Radio From App</Link> */}
+
 
         <Switch>
           <Route path="/" component={Home} exact/>
+          <Route path="/" component={NameForm} exact/>
           <Route path="/radio" component={Radio} />
         </Switch>  
       </Router>
---
+++ src/NameForm/NameForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NameForm = () => {
+
+    return (
+        <div>Name Form</div>
+    )
+}
+
+export default NameForm 
