4ada70c2636d1ec4c18a863d1f4e28f7c1c3c421
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-31 19:20:00 -0600

    build out a header and added to all the right places

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,10 +6,12 @@ import Calendar from '../Calendar/Calendar';
 import NavBar from '../NavBar/NavBar';
 import Radio from '../Radio/Radio';
 import { main } from './App.module.css';
+import Header from '../Header/Header';
 
 const App = () => {
   return (
     <div className={main}>
+      <Header />
       <p>Hello World</p>
       <button>within react but outside of Router</button>
       <Router>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { headStyle } from './Header.module.css';
+
+const Header = () => {
+
+    return (
+        <div className={headStyle}>This is the Header</div>
+    )
+}
+
+export default Header;
