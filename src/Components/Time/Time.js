9137e0257a1739fb2b064cd296588efc9c0f2329
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-01 22:49:52 -0600

    brought in TimePicker comp and it works

--- src/Components/Time/Time.js
+++ src/Components/Time/Time.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
+import TimePicker from 'react-time-picker'
 import { head, body } from '../Universal.module.css'
 import styles from './Time.module.css'
 
 const Time = () => {
+    const [value, onChange] = useState('00:00')
     return (
         <div className={body}>
             <h2 className={head}>Time</h2>
+            <TimePicker
+                clockIcon={null}
+                onChange={onChange}
+                value={value}
+            />
         </div>
     )
 }
