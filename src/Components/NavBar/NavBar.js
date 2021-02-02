aede7a35b2e09185c330c89ed5830af6e69ce8d2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-01 22:34:11 -0600

    swapped Links with NavLinks

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { active, navStyle, linkStyle } from './NavBar.module.css';
 
 const NavBar = () => {
-    const location = useLocation();
 
     return (
         <div className={navStyle}>
-            <Link className={linkStyle} to="/">Home</Link>
-            <Link className={location.pathname === "/calendar" ? `${active} ${linkStyle}` : linkStyle} to="/calendar">Calendar</Link>
-            <Link className={location.pathname === "/color" ? `${active} ${linkStyle}` : linkStyle} to="/color">Color</Link>
-            <Link className={location.pathname === "/date" ? `${active} ${linkStyle}` : linkStyle} to="/date">Date</Link>
-            <Link className={location.pathname === "/radio" ? `${active} ${linkStyle}` : linkStyle} to="/radio">Radio</Link>
-            <Link className={location.pathname === "/text" ? `${active} ${linkStyle}` : linkStyle} to="/text">Text</Link>
-            <Link className={location.pathname === "/time" ? `${active} ${linkStyle}` : linkStyle} to="/time">Time</Link>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/" exact>Home</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/calendar">Calendar</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/color">Color</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/date">Date</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/radio">Radio</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/text">Text</NavLink>
+            <NavLink activeClassName={`${active} ${linkStyle}`} className={linkStyle} to="/time">Time</NavLink>
         </div>
     )
 }
