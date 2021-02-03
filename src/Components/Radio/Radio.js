02ea9152c81d4f84b50467c67aef22d9a92c9ae6
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-02 20:14:43 -0600

    reimported form and added submit button, working a little screwy

--- src/Components/Radio/Radio.js
+++ src/Components/Radio/Radio.js
@@ -13,45 +13,45 @@ const Radio = () => {
             <form>
                 <div className="form-check">
                     <label>
-                        <input
+                    <input
                         type="radio"
                         name="react-tips"
                         value="option1"
                         checked={true}
                         className="form-check-input"
-                        />
-                        Option 1
+                    />
+                    Option 1
                     </label>
                 </div>
                 <div className="form-check">
-                <label>
+                    <label>
                     <input
-                    type="radio"
-                    name="react-tips"
-                    value="option2"
-                    className="form-check-input"
+                        type="radio"
+                        name="react-tips"
+                        value="option2"
+                        className="form-check-input"
                     />
                     Option 2
-                </label>
+                    </label>
                 </div>
                 <div className="form-check">
-                <label>
+                    <label>
                     <input
-                    type="radio"
-                    name="react-tips"
-                    value="option3"
-                    className="form-check-input"
+                        type="radio"
+                        name="react-tips"
+                        value="option3"
+                        className="form-check-input"
                     />
                     Option 3
-                </label>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary mt-2" type="submit">
+                    Submit
+                    </button>
                 </div>
 
-                <div className="form-group">
-                <button className="btn btn-primary mt-2" type="submit">
-                    Save
-                </button>
-                </div>
-            </form>
+                </form>
         </div>
     )
 }
