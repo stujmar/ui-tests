8f187f8fdf13bb714d5d7038535e6cc1ffaa440d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-02 20:11:44 -0600

    brought in radio form and am refactoring

--- src/Components/Radio/Radio.js
+++ src/Components/Radio/Radio.js
@@ -1,20 +1,57 @@
 import React from "react"
 import { head, body } from '../Universal.module.css'
-import CashRadioButtons from "./CashedRadioButtons"
+
 import styles from './Radio.module.css'
 
 const Radio = () => {
 
-
-
-    
     return (
         <div className={body} >
             <h2 className={head}>    
                 Radio
             </h2>
-            <CashRadioButtons />
+            <form>
+                <div className="form-check">
+                    <label>
+                        <input
+                        type="radio"
+                        name="react-tips"
+                        value="option1"
+                        checked={true}
+                        className="form-check-input"
+                        />
+                        Option 1
+                    </label>
+                </div>
+                <div className="form-check">
+                <label>
+                    <input
+                    type="radio"
+                    name="react-tips"
+                    value="option2"
+                    className="form-check-input"
+                    />
+                    Option 2
+                </label>
+                </div>
+                <div className="form-check">
+                <label>
+                    <input
+                    type="radio"
+                    name="react-tips"
+                    value="option3"
+                    className="form-check-input"
+                    />
+                    Option 3
+                </label>
+                </div>
 
+                <div className="form-group">
+                <button className="btn btn-primary mt-2" type="submit">
+                    Save
+                </button>
+                </div>
+            </form>
         </div>
     )
 }
