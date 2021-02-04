e32b792c93751316e6cd4791065577cf2d33c164
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-03 22:06:45 -0600

    added source for react-time-picker and added universal source class

--- src/Components/Time/Time.js
+++ src/Components/Time/Time.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import TimePicker from 'react-time-picker'
-import { head, body } from '../Universal.module.css'
+import { head, body, source} from '../Universal.module.css'
 import styles from './Time.module.css'
 
 const Time = () => {
@@ -8,6 +8,7 @@ const Time = () => {
     return (
         <div className={body}>
             <h2 className={head}>Time</h2>
+            <a className={source} href="https://www.npmjs.com/package/react-time-picker">react-time-picker</a><br/>
             <TimePicker
                 disableClock={true}
                 onChange={onChange}
