b70163207d967f94f2431339c7b46e3b99d59e45
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-06 17:44:08 -0600

    created a controlled component for the input field

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,21 +1,31 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { head, body } from '../Universal.module.css'
 import styles from './Search.module.css'
 
 const Search = () => {
 
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState('');
 
     const handleChange = (e) => {
         console.log(e.target.value);
+        setQuery(e.target.value);
     }
+
+    useEffect(() => {
+        console.log(query, "from useEffect");
+    }, [query])
 
     return (
         <div className={body}>
             <h2 className={head}>Search</h2>
             <form>
-                <label htmlFor="symbol">Type any stock symbol. </label>
-                <input id="symbol" type="text" onChange={handleChange}></input>
+                <label htmlFor="symbol">Type any stock symbol.
+                    <input 
+                        id="symbol" 
+                        type="text" 
+                        value={query}
+                        onChange={handleChange}></input>
+                 </label>
             </form>
         </div>
     )
