be3605d8fe5cc6fc716f4765eeef7c676bdf6642
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-05 22:22:11 -0600

    put simple Calendar picker into calendar route

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -4,14 +4,19 @@ import {Prompt} from 'react-router-dom';
 import { head, body } from '../Universal.module.css'
 import styles from './Calendar.module.css'
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const Calendar = () => {
     const [name, setName] = useState("");
-    
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
         <div className={body}>
         <h2 className={head}>
             Calendar
         </h2>
+        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
         </div>
     )
 }
