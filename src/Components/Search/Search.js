7d21d0ef9d453aa3ad1cabe3e78d8b444373cc14
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-06 16:29:21 -0600

    brought state into search comp along with an input dialogue

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,12 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { head, body } from '../Universal.module.css'
 import styles from './Search.module.css'
 
 const Search = () => {
 
+    const [query, setQuery] = useState("");
+
+
     return (
         <div className={body}>
             <h2 className={head}>Search</h2>
+            <input></input>
         </div>
     )
 }
