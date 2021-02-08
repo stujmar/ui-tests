a4d689cbabb813d834b5bec2a55d17326288a045
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-07 18:50:36 -0600

    built out sections in results, added watchlist button

--- src/Components/Search/StockResult.js
+++ src/Components/Search/StockResult.js
@@ -1,12 +1,16 @@
 import React from "react"
-import { head, body } from '../Universal.module.css'
+import { head, body, leftSection, rightSection } from './StockResult.module.css'
 import styles from './StockResult.module.css'
 
 const StockResult = ({data}) => {
     return (
         <div className={body}>
-            <div className={head}>
-                {`Name: ${data.name.substring(0,20)}...`}
+            <div className={leftSection}>
+            <div>{`Ticker: ${data.ticker}`}</div>
+                <div>{`Name: ${data.name.substring(0,20)}...`}</div>
+            </div>
+            <div className={rightSection}>
+                <button>WATCHLIST</button>
             </div>
         </div>
     )
