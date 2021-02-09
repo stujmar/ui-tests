fa17613229c3b3d6d5f08be91f7342b14e3d1bca
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-08 23:19:43 -0600

    installed primeflex

--- src/Components/Color/Color.js
+++ src/Components/Color/Color.js
@@ -3,15 +3,29 @@ import { head, body } from '../Universal.module.css'
 // import styles from './Color.module.css'
 import {ColorPicker} from 'primereact/colorpicker';
 
+//Imports for PrimeReact color picker.
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
+// import 'primeflex/primeflex.css';
+import '../../index.css';
+
+
 const Color = () => {
-    const [color, setColor] = useState('blue');
-    const [color2, setColor2] = useState('');
+    const [color, setColor] = useState(null);
+    const [color2, setColor2] = useState('1976D2');
+
+    const handlePickOne = (e) => {
+        setColor(e.target.value)
+        console.log(e.target.value);
+    }
+
     return (
         <div className={body}>
             <h2 className={head}>
                 Color
             </h2>
-            <ColorPicker value={color} onChange={(e) => setColor(e.value)} />
+            <ColorPicker value={color} onChange={handlePickOne} />
             <ColorPicker format="rgb" value={color2} onChange={(e) => setColor2(e.value)} />
         </div>
     )
