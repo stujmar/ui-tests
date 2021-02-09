caef8b714322edd1682a152f61439fb33e493275
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-08 23:08:24 -0600

    adde second color picker from prime react same issues persist

--- src/Components/Color/Color.js
+++ src/Components/Color/Color.js
@@ -4,13 +4,15 @@ import { head, body } from '../Universal.module.css'
 import {ColorPicker} from 'primereact/colorpicker';
 
 const Color = () => {
-    const [color, setColor] = useState('');
+    const [color, setColor] = useState('blue');
+    const [color2, setColor2] = useState('');
     return (
         <div className={body}>
             <h2 className={head}>
                 Color
             </h2>
             <ColorPicker value={color} onChange={(e) => setColor(e.value)} />
+            <ColorPicker format="rgb" value={color2} onChange={(e) => setColor2(e.value)} />
         </div>
     )
 }
