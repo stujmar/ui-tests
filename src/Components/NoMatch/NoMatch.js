96223b0f6dffcff6febfac58ff94b7867928800f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-02-09 22:40:21 -0600

    build out NoMatch folder structure and comp structure

---
+++ src/Components/NoMatch/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Styles from './NoMatch.module.css'
+
+const NoMatch = () => {
+    return (
+        <div>
+            This is probably a 404
+        </div>
+    )
+}
+
+export default NoMatch
