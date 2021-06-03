3ce94a8980c79d1e465bf64c2150715f6a09c9a9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-02 19:46:52 -0500

    [sm] mapped the employee data into the svgmap component using useEffect and useState

--- src/Components/SvgMap/SvgMap.js
+++ src/Components/SvgMap/SvgMap.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {data} from './profileData';
 
 const SvgMap = () => {
+    const [ employees, setEmployees ] = useState([]);
+    useEffect(() => {
+        setEmployees(
+                data.map((profile) => {
+                    return <div id={profile.id}>{profile.name}</div>
+                })
+            )
+        },[])
+
     return (
-        <>This is where the svg map will go</>
+        <div>
+            <h2>This is where the svg map will go</h2>
+            {employees}
+        </div>
     )
 }
 
