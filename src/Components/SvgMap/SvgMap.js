59a35501bfd42985747ec4ba1f2c93a6ea3b4485
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-02 20:37:45 -0500

    [sm] got deleting and reseting employee data to work

--- src/Components/SvgMap/SvgMap.js
+++ src/Components/SvgMap/SvgMap.js
@@ -3,18 +3,37 @@ import {data} from './profileData';
 import { wrapper } from './SvgMap.module.css';
 
 const SvgMap = () => {
+    const [ employeeData, setEmployeeData ] = useState([])
     const [ employees, setEmployees ] = useState([]);
+
+    const deleteUser = (id) => {
+        console.log(id, employeeData)
+        setEmployeeData(employeeData.filter(person => person.id !== id));
+    }
+
+    const reload = () => {
+        setEmployeeData(data);
+    }
+
+    useEffect(() => {
+        setEmployeeData(data);
+    },[]) 
+
     useEffect(() => {
         setEmployees(
-                data.map((profile) => {
-                    return <div id={profile.id}>{profile.name}</div>
+                employeeData.map((profile) => {
+                    return <div key={profile.id} id={profile.id}>{profile.name}
+                    <button onClick={() => {deleteUser(profile.id)}}>X</button>
+                    </div>
                 })
             )
-        },[])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        },[employeeData])
 
     return (
         <div className={wrapper}>
             <h2>This is where the svg map will go</h2>
+            <button type="button" onClick={reload}>RELOAD</button>
             {employees}
         </div>
     )
